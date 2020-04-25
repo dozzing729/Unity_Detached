b5f1e97132209e56255031023e4907886c0c74f8
GrandNewStart <hbjw123@naver.com>
2020-04-25 11:04:58 +0900

    project upload

---
+++ Project Files/Assets/Scripts/JumpPadController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPadController : MonoBehaviour
+{
+    public Rigidbody2D playerRigidbody;
+    public GameObject playerCheck;
+    public GameObject jumpPad;
+    public SwitchController switchController;
+    private Vector2 jumpPadPositionBefore;
+    private Vector2 jumpPadPositionAfter;
+    public float playerCheckRadius;
+    public float jumpPower;
+    private bool playerOnPad;
+    private bool activated;
+
+    private void Start()
+    {
+        jumpPadPositionBefore = jumpPad.transform.position;
+        jumpPadPositionAfter = jumpPadPositionBefore;
+        jumpPadPositionAfter.y += 1;
+        activated = false;
+    }
+
+    private void Update()
+    {
+        PlayerCheck();
+        ActivateJumpPad();
+    }
+
+    private void PlayerCheck()
+    {
+        playerOnPad = Physics2D.OverlapCircle(playerCheck.transform.position, playerCheckRadius, LayerMask.GetMask("Player"));
+    }
+
+    private void ActivateJumpPad()
+    {
+        if (switchController.getActivated() && playerOnPad && !activated)
+        {
+            playerRigidbody.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            activated = true;
+            jumpPad.gameObject.transform.position = jumpPadPositionAfter;
+            Invoke("Deactivate", 0.5f);
+        }
+    }
+
+    private void Deactivate() { 
+        activated = false;
+        jumpPad.gameObject.transform.position = jumpPadPositionBefore;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(playerCheck.transform.position, playerCheckRadius);
+    }
+}
